e7d5230d26a288b158d413d3fd71639c0cda7f94
Aaron Jensen <aaronjensen@gmail.com>
2016-01-24 13:29:39 -0800

    Add spaceline back

--- init.el
+++ init.el
@@ -73,7 +73,6 @@ values."
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages
    '(
-     spaceline
      )
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
    ;; are declared in a layer which is not a member of
